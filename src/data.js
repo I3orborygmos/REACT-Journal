698843be10ab05d077b8042e4edf98f110791e7d
I3orborygmos <117126600+I3orborygmos@users.noreply.github.com>
2024-01-15 17:20:09 -0800

    Update data.js
    fixing typos

--- src/data.js
+++ src/data.js
@@ -6,7 +6,7 @@ export default [
         mapLink:'https://maps.app.goo.gl/ZTn8CeRNzwgXRr3a9',
         title:'Yosemite',
         travelDate:'10/22/2023',
-        description:'Went for a week in October just before it started getting cold. Althought alot of the waterfalls were somewhat dry to due cold and climate it did not take away from the amzing veiws and hikes I did with my family. Some notible memories were biking along Yosemite Valley and seeing the view from Glacier Point.  ',
+        description:'Went for a week in October just before it started getting cold. Although a lot of the waterfalls were somewhat dry due to cold and climate it did not take away from the amazing views and hikes I did with my family. Some notable memories were biking along Yosemite Valley and seeing the view from Glacier Point.  ',
     },
     {
         id:2,
@@ -15,7 +15,7 @@ export default [
         mapLink:'https://maps.app.goo.gl/fCAiabrEWu7c2BYs7',
         title:'Yellowstone',
         travelDate:'8/2008',
-        description:'Went to Yellow stone close to the begining of August right before school started. It was mating season for the Buffalo so me and my family saw many of them both off and on the roads surrounding Yellowstone. Most memorable part was seeing the gysters and the sulfur pools. However nothing compares to seeing a small female Park Ranger scare off a Hungry bear from a campsite. Still talked about among my family to this day.',
+        description:'Went to Yellow Stone close to the beginning of August right before school started. It was mating season for the Buffalo so me and my family saw many of them both off and on the roads surrounding Yellowstone. Most memorable part was seeing the gysters and the sulfur pools. However nothing compares to seeing a small female Park Ranger scare off a Hungry bear from a campsite. Still talked about among my family to this day.',
     },
     {
         id:3,
@@ -24,7 +24,7 @@ export default [
         mapLink:'https://maps.app.goo.gl/wBspKtMKXPQsbSoB7',
         title:'Grand Canyon',
         travelDate:'7/2003',
-        description:'Went to the Grand Canyon as a kid. Being honest I do not really remember much other then I got some really good jerky on Route 66. Definetly have to go back there now that I am much older.',
+        description:'Went to the Grand Canyon as a kid. Being honest I do not really remember much other then I got some really good jerky on Route 66. Definitely have to go back there now that I am much older.',
     }
 
 ]
